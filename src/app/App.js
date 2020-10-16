34bf8c949303d6d496ab78e9f515ab5acda95b2b
pgonzalo <pintadogonzalo2@gmail.com>
2020-10-16 16:24:18 -0300

    mern-Stack-task 1610

---
+++ src/app/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="ligth-blue darken-4">
+                    <div className="container">
+                        <a className="brand-logo" href="/">MERN
+                        Stack</a>
+                    </div>
+                </nav>
+            </div>
+            
+        )
+    }
+}
+
+export default App;
